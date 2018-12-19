7f15fd3c5e3dbba4977d74edadfe749bc048bec8
duanxqing <2045538093@qq.com>
2018-12-19 21:29:18 +0800

    哈哈哈

--- app/src/main/java/com/example/duanxiaqingqq/myapplication/MainActivity.java
+++ app/src/main/java/com/example/duanxiaqingqq/myapplication/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //lkoonji
+
+        //  我是 胖墩墩
     }
 }
